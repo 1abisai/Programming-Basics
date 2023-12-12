7eab0ab2b001f697aa836b9f7bf216d4fd11d004
1Abisai <abisai.esc@gmail.com>
2023-12-12 15:20:34 -0700

    DOM

---
+++ css/buttonclicker.js
@@ -0,0 +1,13 @@
+// Logout and Login 
+function change(button1) {
+    if (button1.innerText === 'Login') {
+        button1.innerText = "Logout"
+    }
+    else {
+        button1.innerText = 'Login'
+    }
+}
+// remove definition
+function remove(button2){
+    button2.remove(add)
+}
